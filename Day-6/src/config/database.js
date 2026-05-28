0f5b071a7c00180903a0289b04e19f28d63ebee3
Ashutosh Sahu <ashutoshsahuit@gmail.com>
2026-05-28 16:42:19 +0530

    ,

--- Day-6/src/config/database.js
+++ Day-6/src/config/database.js
@@ -6,6 +6,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"])
 dotenv.config()
 
 function connectToDB(){
+    
     mongoose.connect("mongodb+srv://ashutoshsahuit_db_user:DSGQ3PzeLoOVCkHr@cluster0.bnehqez.mongodb.net/day10")
     .then(()=>{
         console.log("server is connected to database");
