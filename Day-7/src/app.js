f4dc1ae2cae103b3616172ca1c9fa5f0126c3dd4
Ashutosh Sahu <ashutoshsahuit@gmail.com>
2026-05-31 15:17:58 +0530

    chal gaya

--- Day-7/src/app.js
+++ Day-7/src/app.js
@@ -22,7 +22,7 @@ app.post("/notes",async(req,res)=>{
 })
 
 app.get("/notes",async(req,res)=>{
-    const notes = await noteModel.find
+    const notes = await noteModel.find()
 
     res.status(200).json({
         message:"Notes fetched successfully",
