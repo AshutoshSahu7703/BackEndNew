0edd76e92bd689ed94a6708ef3ef41120b4c8ff1
Ashutosh Sahu <ashutoshsahuit@gmail.com>
2026-07-04 16:35:11 +0530

    Controller-done

---
+++ Day-11-Insta-Clone/src/controller/auth.controller.js
@@ -0,0 +1,106 @@
+
+const userModel=require("../models/user.model")
+
+const crypto=require("crypto")
+
+const jwt = require("jsonwebtoken")
+
+async function registerController(req,res){
+
+    const {name,email,password,bio,profileImage}=req.body
+
+    const isUserAlreadyExist= await userModel.findOne({
+        $or:[
+            {name},
+            {email}
+        ]
+    })
+
+    if(isUserAlreadyExist){
+        return res.status(409).json({
+            message:"User already exist"+(isUserAlreadyExist.email === email?"with this email":"with this username")
+        })
+    }
+    
+    const hash = await crypto.createHash("md5").update(password).digest("hex")
+
+    const user = await userModel.create({
+        name,
+        email,
+        password:hash,
+        bio,
+        profileImage
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+    },process.env.JWT_SECRET,{expiresIn:"1d"})
+
+    res.cookie("jwt-token",token)
+
+    res.status(200).json({
+        message:"User registered Successfully",
+        user:{
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                bio: user.bio,
+                profileImage: user.profileImage
+        }
+    })
+    
+}
+
+async function logInController(req,res){
+    const {email,name,password}=req.body
+
+    const user = await userModel.findOne({
+        $or:[
+        {
+            name:name
+        },
+        {
+            email:email
+        }
+    ]
+    })
+
+    if(!user){
+        return res.status(404).json({
+            message:"user not resgistered yet"
+        })
+    }
+
+    const hash = await crypto.createHash("md5").update(password).digest("hex")
+
+    const isPasswordValid= hash === user.password
+
+    if(!isPasswordValid){
+        res.status(401).json({
+            message:"Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET,{expiresIn:"1d"})
+
+    res.cookie("jwt-token",token)
+
+    res.status(201).json({
+        message:"user logged in",
+        user:{
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            bio: user.bio,
+            profileImage: user.profileImage
+        }
+    })
+}
+
+module.exports={
+    registerController,
+    logInController
+}
+
--- Day-11-Insta-Clone/src/routes/auth.routes.js
+++ Day-11-Insta-Clone/src/routes/auth.routes.js
@@ -1,98 +1,11 @@
 const express = require("express")
 
-const userModel=require("../models/user.model")
-
-const crypto=require("crypto")
-
-const jwt = require("jsonwebtoken")
+const authController=require("../controller/auth.controller")
 
 const authRouter = express.Router()
 
-authRouter.post("/register",async (req,res)=>{
+authRouter.post("/register",authController.registerController)
 
-    const {name,email,password,bio,profileImage}=req.body
-
-    const isUserAlreadyExist= await userModel.findOne({
-        $or:[
-            {name},
-            {email}
-        ]
-    })
-
-    if(isUserAlreadyExist){
-        return res.status(409).json({
-            message:"User already exist"+(isUserAlreadyExist.email === email?"with this email":"with this username")
-        })
-    }
-    
-    const hash = await crypto.createHash("md5").update(password).digest("hex")
-
-    const user = await userModel.create({
-        name,
-        email,
-        password:hash,
-        bio,
-        profileImage
-    })
-
-    const token = jwt.sign({
-        id: user._id,
-    },process.env.JWT_SECRET,{expiresIn:"1d"})
-
-    res.cookie("jwt-token",token)
-
-    res.status(200).json({
-        message:"User registered Successfully",
-        user:{
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                bio: user.bio,
-                profileImage: user.profileImage
-        }
-    })
-    
-})
-
-authRouter.post("/login",async (req,res)=>{
-    const {email,name,password}=req.body
-
-    const user = await userModel.findOne({
-        $or:[
-        {
-            name:name
-        },
-        {
-            email:email
-        }
-    ]
-    })
-
-    if(!user){
-        return res.status(404).json({
-            message:"user not resgistered yet"
-        })
-    }
-
-    const hash = await crypto.createHash("md5").update(password).digest("hex")
-
-    const isPasswordValid= hash === user.password
-
-    if(!isPasswordValid){
-        res.status(401).json({
-            message:"Invalid Password"
-        })
-    }
-
-    const token = jwt.sign({
-        id:user._id
-    },process.env.JWT_SECRET,{expiresIn:"1d"})
-
-    res.cookie("jwt-token",token)
-
-    res.status(201).json({
-        message:"user logged in"
-    })
-})
+authRouter.post("/login",authController.logInController)
 
 module.exports=authRouter
