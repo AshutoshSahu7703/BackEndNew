8e47432adf9b0722ff4ab753a6b2238a04c2b3dd
Ashutosh Sahu <ashutoshsahuit@gmail.com>
2026-07-10 13:34:37 +0530

    image-kit-done

--- Day-11-Insta-Clone/src/controller/post.controller.js
+++ Day-11-Insta-Clone/src/controller/post.controller.js
@@ -1,8 +1,23 @@
 const postModel=require("../models/post.model")
 
-async function createPost(req,res){
-    console.log(req.body);
+const ImageKit=require("@imagekit/nodejs")
+
+const imagekit=new ImageKit({
+    privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,   // ye undefined aa raha hai
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+})
+
+async function createPost(req, res) {
+    console.log("Body:", req.body);  
+    console.log("File:", req.file); 
     
+    const file=await imagekit.files.upload({
+        file:await ImageKit.toFile(Buffer.from(req.file.buffer),'file'),
+        fileName:"Test"
+    })
+
+    res.send(file)
 }
 
 module.exports={
